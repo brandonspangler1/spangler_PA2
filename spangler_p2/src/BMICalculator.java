27b353c3e68e680c3d3fc32556612118a0245725
Brandon Spangler <brandonspangler@knights.ucf.edu>
2020-01-31 02:43:57 -0600

    BMI setup

--- spangler_p2/src/BMICalculator.java
+++ spangler_p2/src/BMICalculator.java
@@ -1,4 +1,86 @@
 
+
+/*
+ * allow user to select type
+ * 		prompt until valid input
+ * read in user weight and height
+ * 		exit if negative weight or height
+ * calculates BMI
+ * 		use correct formula
+ * display BMI categories and values
+ * 		data from National Heart Lung and Blood Institute
+ * 
+ */
+
+
+
 public class BMICalculator {
 
+	public static void main(String[] args) {
+			BMICalculator app = new BMICalculator();
+		    app.readUserData();
+		    app.calculateBmi();
+		    app.displayBmi();
+	}
+	
+	
+	public readUserData() {
+		readUnitType
+		readMeasurmentData
+	}
+	
+	private readUnitType() {
+		
+	}
+	
+	private readMeasurementData() {
+		readMetricData
+		readImperialData
+	}
+	
+	private readMetricData() {
+		
+	}
+	
+	private readImperialData() {
+		
+	}
+	
+	public calculateBmi() {
+		calculateBmiCategory
+	}
+	
+	private calculateBmiCategory() {
+		
+	}
+	
+	public displayBmi() {
+		
+	}
+	
+	public getWeight() {
+		
+	}
+	
+	private setWeight() {
+		
+	}
+	
+	public getHeight() {
+		
+	}
+	
+	private setHeight() {
+		
+	}
+	
+	public getBmi() {
+		
+	}
+	
+	public getBmiCategory() {
+		
+	}
+	
+	
 }
