5b2e57106ce9903ec70f51e1b9f6abd24b1194bb
Brandon Spangler <brandonspangler@knights.ucf.edu>
2020-01-30 21:27:08 -0600

    Encryption Work

--- spangler_p1/src/Application.java
+++ spangler_p1/src/Application.java
@@ -1,4 +1,43 @@
+import java.util.Scanner;
+
+/*
+ * scan four digit number from user
+ * encrypt number
+ * 		call encrypter
+ * decrypt number
+ * 		call decrypter
+ */
+
 
 public class Application {
+	
+	public static void main(String[] args) {
+		
+		Encrypter num = new Encrypter();
+		
+		Scanner kb = new Scanner(System.in);
+		String choice;
+		
+		while(true) {
+			System.out.println("What would you like to do?");
+			System.out.println("\tEnter 1 for encryter");
+			System.out.println("\tEnter 2 for decrypter");
+			System.out.println("\tEnter 3 to exit");
+			
+			choice = kb.nextLine();
+			
+			if (Integer.parseInt(choice) == 1) {
+				num.doEncryption();
+			}
+			else if (Integer.parseInt(choice) == 2) {
+				
+			}
+			else if (Integer.parseInt(choice) == 3) {
+				break;
+			}
+		}
+		
+		kb.close();
+	}
 
 }
--- spangler_p1/src/Encrypter.java
+++ spangler_p1/src/Encrypter.java
@@ -1,4 +1,96 @@
+import java.util.Scanner;
+
+/*
+ * Pseudocode
+ * scan four digit number from user
+ * encrypt each digit
+ * 		add 7 to each number
+ * 		get remainder after dividing by 10
+ * 		swap first with third 
+ * 		swap second with fourth
+ * print encrypted number
+ *  
+ */
+
 
 public class Encrypter {
 
+	final int ENCRYPTIONLENGTH = 4;
+	
+	public void doEncryption() {
+	
+		String numToEncrypt = getNumFromUser();
+		
+		int nums[] = add7(numToEncrypt);
+		
+		nums = mod10(nums);
+		
+	
+		
+		
+		
+		for(Integer x : nums) {
+			System.out.println(x);
+		}
+		
+	
+	}
+	
+	
+	
+	
+	public String getNumFromUser() {
+		Scanner num = new Scanner(System.in);
+		String numToEncrypt;
+		
+		
+		while(true) {
+			
+			System.out.println("Enter your number: ");
+			numToEncrypt = num.nextLine();
+			
+			if (numToEncrypt.length() > ENCRYPTIONLENGTH || numToEncrypt.length() < ENCRYPTIONLENGTH || numToEncrypt.charAt(0) == '-') {
+				System.out.println("Enter a vaild " + ENCRYPTIONLENGTH + " digit number!");
+				numToEncrypt = null;
+				continue;
+			}
+
+			try {
+				Integer.parseInt(numToEncrypt);
+			} catch (Exception e) {
+				System.out.println("Enter a vaild " + ENCRYPTIONLENGTH + " digit number!");
+				numToEncrypt = null;
+				continue;
+			}
+			
+			if (numToEncrypt != null) {
+				break;
+			}
+			
+		}
+		
+		return numToEncrypt;
+	}
+	
+	public int[] add7(String numToEncrypt) {
+		int nums[] = new int[ENCRYPTIONLENGTH];
+		
+		for (int i = 0; i < ENCRYPTIONLENGTH; i++) {
+			nums[i] = Integer.parseInt(String.valueOf(numToEncrypt.charAt(i)));
+			nums[i] += 7;
+		}
+		
+		return nums;
+	}
+	
+	public int[] mod10(int[] nums) {
+		
+		for (int i = 0; i < ENCRYPTIONLENGTH; i++) {
+			nums[i] = nums[i]%10;
+		}
+		
+		return nums;
+		
+	}
+	
 }
